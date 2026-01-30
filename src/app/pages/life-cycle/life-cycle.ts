a356766c91d48b150b707a5ae953f06a562d8d02
PedroMakengo <pedromakengomiguel@gmail.com>
2026-01-30 22:55:56 +0100

    Fix: Add life-cycle

---
+++ src/app/pages/life-cycle/life-cycle.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-life-cycle',
+  imports: [],
+  templateUrl: './life-cycle.html',
+  styleUrl: './life-cycle.css',
+})
+export class LifeCycle {
+
+}
