e30de8b5a44290b8f364da295f06f99e63b4d78a
PedroMakengo <pedromakengomiguel@gmail.com>
2026-01-31 13:46:04 +0100

    Fix: Adicionado o meu crud

--- src/app/pages/employee-crud/employee-crud.ts
+++ src/app/pages/employee-crud/employee-crud.ts
@@ -1,5 +1,5 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,19 +9,57 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './employee-crud.html',
   styleUrl: './employee-crud.css',
 })
-export class EmployeeCrud {
+export class EmployeeCrud implements OnInit {
   empObje: Employee = new Employee();
 
+  // empList: Employee[] = [];
+  empList = signal<Employee[]>([]);
   http = inject(HttpClient);
   url: string = 'https://68397a596561b8d882b08dd5.mockapi.io/api/v1/employee';
 
-  onSaveEmployee() {
-    debugger;
-    this.http.post(this.url, this.empObje).subscribe({
+  ngOnInit(): void {
+    this.getAllEmployees();
+  }
+
+  onEditEmployee(obj: Employee) {
+    const strEmp = JSON.stringify(obj);
+    const empObj = JSON.parse(strEmp);
+    this.empObje = empObj;
+  }
+
+  getAllEmployees() {
+    this.http.get(this.url).subscribe({
       next: (result: any) => {
-        debugger;
+        this.empList.set(result);
       },
     });
+  }
+
+  onSaveEmployee() {
+    this.http.post(this.url, this.empObje).subscribe({
+      next: () => {
+        this.getAllEmployees();
+      },
+    });
+  }
+
+  onUpdateEmployee() {
+    this.http.put(this.url + '/' + this.empObje.id, this.empObje).subscribe({
+      next: () => {
+        this.getAllEmployees();
+      },
+    });
+  }
+
+  onDeleteEmployee(id: number) {
+    const bConfirm = confirm('Pretendes mesmo remover este documento?');
+    if (bConfirm) {
+      this.http.delete(this.url + '/' + id).subscribe({
+        next: () => {
+          this.getAllEmployees();
+        },
+      });
+    }
   }
 }
 
